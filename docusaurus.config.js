9dc573245564accc391f42a1f3314bc39905377c
Manuel Meurer <manuel@krautcomputing.com>
2021-06-24 22:45:43 +0200

    fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/uplinkhq/knowledge-base/edit/main/docs/',
+          editUrl: 'https://github.com/uplinkhq/knowledge-base/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
