5915ec68415977859f39f4cf1d3e6c42b4ea9dee
Manuel Meurer <manuel@krautcomputing.com>
2021-06-28 04:26:27 +0200

    fix links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
         },
         {
           label: 'Blog',
-          href: 'blog',
+          href: '/blog',
         },
         {
           label: 'Slack',
@@ -64,7 +64,7 @@ module.exports = {
             },
             {
               label: 'Blog',
-              href: 'blog',
+              href: '/blog',
             },
             {
               label: 'Slack',
