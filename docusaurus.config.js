24745533195bbaf8164761ca868af331b914e638
Manuel Meurer <manuel@krautcomputing.com>
2021-07-20 13:23:14 +0200

    add algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,6 +95,10 @@ module.exports = {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
+    algolia: {
+      apiKey: process.env.ALGOLIA_API_KEY,
+      indexName: 'uplink',
+    },
   },
   presets: [
     [
